0899a20e9bbd36bfc32e930cf825ecfb88078d5b
Austin McDowell <austintmcdowell@berkeley.edu>
2017-09-12 23:08:40 -0400

    worked on mandelbrot script

--- mandelbrot.py
+++ mandelbrot.py
@@ -7,16 +7,7 @@ def main(S, N):
     S : number of iteration steps for each point
     N : grid size """
 
-    x, y = make_grid(N)
-    # making grids
-
-def check_man(z):
-
-    """ checks z to see if it is in the Mandelbrot set """
-
-    abs_z = sqrt( (z*z.conjugate()) )
-
-    
+    point_count(N, S)
 
 def make_grid(N):
     
@@ -28,15 +19,44 @@ def make_grid(N):
     
     return x, y
 
-def point_count(x, y):
+def point_count(N, S):
 
     """ for each point, counts how many times z' is in the set
     given xi and yi """
 
-    for xi in x:
-        for yi in y:
+    x, y = make_grid(N)
 
-            z = 0
+    xc, yc = np.zeros_like(x), np.zeros_like(y)
+
+    
+    for (xi, i) in zip(x, xrange(N)):
+        for (yi, j) in zip(y, xrange(N)):
+
+            z = 0 ; s = 0
             c = complex( xi , yi ) 
-            
-            
+            abs_z = np.sqrt( z*z.conjugate() )
+            eps = 10**-12
+
+            for k in xrange(S):
+                
+                if abs_z > 2:
+                    break
+                else:
+                    z_prim = z*z + c
+                    abs_z = np.sqrt( z_prim*z_prim.conjugate() )
+                    z = z_prim 
+                    s += 1
+
+            if abs_z < 2:
+                xc[i] += 1
+                yc[j] += 1
+                
+
+    return xc, yc
+
+
+
+
+
+
+
