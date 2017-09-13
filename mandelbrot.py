0f2c25aa048bb961eefcbb9333d54d7c8882c29c
austin mcdowell <austin@172-16-229-82.DYNAPOOL.NYU.EDU>
2017-09-13 11:43:00 -0400

    finished mandelbrot

--- mandelbrot.py
+++ mandelbrot.py
@@ -7,7 +7,25 @@ def main(S, N):
     S : number of iteration steps for each point
     N : grid size """
 
-    point_count(N, S)
+    z_binary, z_density = point_count(N, S)
+
+    extent = [-2, 2, -2, 2]
+    plt.imshow(z_binary, extent=extent, cmap='Greys')
+    plt.show()
+    plt.close('all')
+
+    log_zd = np.log10(z_density)
+    plt.imshow(z_density, extent=extent, cmap='jet')
+    plt.show()
+    plt.close('all')
+
+    log_zd = np.log10(z_density)
+    plt.imshow(log_zd, extent=extent, cmap='jet')
+    plt.show()
+    plt.close('all')
+
+
+    
 
 def make_grid(N):
     
@@ -16,7 +34,8 @@ def make_grid(N):
 
     x = np.linspace(-2. , 2 , N)
     y = np.linspace(-2. , 2 , N)
-    
+    # two evenly spaced grids from -2 to 2
+
     return x, y
 
 def point_count(N, S):
@@ -27,18 +46,21 @@ def point_count(N, S):
     x, y = make_grid(N)
 
     xc, yc = np.zeros_like(x), np.zeros_like(y)
-
+    # grids for holding result of mandelbrot check
     
+    z_binary = np.zeros( (N, N) )
+    z_density = np.zeros( (N, N) )
+
     for (xi, i) in zip(x, xrange(N)):
         for (yi, j) in zip(y, xrange(N)):
 
             z = 0 ; s = 0
             c = complex( xi , yi ) 
             abs_z = np.sqrt( z*z.conjugate() )
-            eps = 10**-12
+            # initial values for z, c, |z|, and step count
 
             for k in xrange(S):
-                
+
                 if abs_z > 2:
                     break
                 else:
@@ -46,13 +68,14 @@ def point_count(N, S):
                     abs_z = np.sqrt( z_prim*z_prim.conjugate() )
                     z = z_prim 
                     s += 1
-
-            if abs_z < 2:
-                xc[i] += 1
-                yc[j] += 1
+                    z_density[j, i] += 1
                 
 
-    return xc, yc
+
+            if abs_z < 2:
+                z_binary[j, i] = 1
+                
+    return z_binary, z_density
 
 
 
